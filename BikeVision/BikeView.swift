9802ae42c99e2cf3f15a2e194df3f0d688ce2e22
Ash <developer@ylabz.com>
2025-02-23 17:23:34 -0800

    Update Mock

--- BikeVision/BikeView.swift
+++ BikeVision/BikeView.swift
@@ -5,10 +5,27 @@
 //  Created by Siamak Ashrafi on 2/23/25.
 //
 import SwiftUI
+import Combine
+
+// A simple mock BLE manager to simulate connected state for previews.
+class MockBLEManager: BLEManager {
+    
+    override init() {
+            super.init()
+            // Provide mock data for previews
+            self.isConnected = true
+            self.bikeName = "Simulated Bike"
+            self.sensorData = "0x1a2b3c"
+        }
+    
+    
+    
+}
 
 struct BikeView: View {
-    @StateObject var bleManager = BLEManager()
-
+    // For real usage, you'd use BLEManager(), but for testing, we can inject a mock.
+    @StateObject var bleManager: BLEManager
+    
     var body: some View {
         VStack(spacing: 20) {
             if bleManager.isConnected {
@@ -27,15 +44,27 @@ struct BikeView: View {
             }
         }
         .onAppear {
-            // BLE scanning starts in BLEManager's init. You can add additional logic here if needed.
+            // In a real app, BLE scanning would start in BLEManager's init.
         }
         .padding()
     }
 }
 
-struct BikeView_Previews: PreviewProvider {
-    static var previews: some View {
-        BikeView()
+// MARK: - Previews
+
+// A wrapper view to inject a mock BLE manager into BikeView for preview purposes.
+struct BikeView_PreviewWrapper: View {
+    @StateObject var mockManager = MockBLEManager() as BLEManager
+    
+    var body: some View {
+        BikeView(bleManager: mockManager)
     }
 }
+
+struct BikeView_Previews: PreviewProvider {
+    static var previews: some View {
+        BikeView_PreviewWrapper()
+    }
+}
+
 
